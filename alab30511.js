030fda77515ec4935ba160f95bf04b58a1a23f76
Dtan713 <s.donaldtan@gmail.com>
2024-05-16 13:03:55 -0400

    some of it

---
+++ alab30511.js
@@ -0,0 +1,34 @@
+function sumArray(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumArray(numbers)); // Output: 15
+
+return[1, 2, 3, 4, 5,];
+console.log(returnthesum);
+
+function longestString(arr) {
+    return arr.reduce((longest, current) => longest.length > current.length ? longest : current, '');
+}
+
+const stringsLongerThan = ['say', 'hello', 'in', 'the', 'morning'];
+console.log(longestString(string)); // Output: "morning"
+
+function stringsLongerThan(arr, length) {
+    return arr.filter(str => str.length > length);
+}
+//test comment
+
+
+
+
+
+
+
+
+
+    
+
+ 
+ 
