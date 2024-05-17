c84e6d3ab06a4475ebb368b58857a59092003f3c
Dtan713 <s.donaldtan@gmail.com>
2024-05-17 11:05:47 -0400

    adding more

--- alab30511.js
+++ alab30511.js
@@ -1,13 +1,13 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-let totalsum = 0
-for (let i= 0; i < numbers.length; i++) { //for loop is iterating through nums
-    //add current number to total
-    //totalsum = totalsum + numbers[i](current number(1))
-    totalsum += numbers[i] // adding to total
-    console.log(`i:${i}, currentNumber: ${numbers[i]} total: ${totalsum}`) //show data in terminal
-}
+// let totalsum = 0
+// for (let i= 0; i < numbers.length; i++) { //for loop is iterating through nums
+//     //add current number to total
+//     //totalsum = totalsum + numbers[i](current number(1))
+//     totalsum += numbers[i] // adding to total
+//     console.log(`i:${i}, currentNumber: ${numbers[i]} total: ${totalsum}`) //show data in terminal
+// }
 
 
 // function sumArray(arr) {
@@ -32,26 +32,44 @@ for (let i= 0; i < numbers.length; i++) { //for loop is iterating through nums
 // }
 
 // sort the array by age
-const people = [
-    { name: "Alice", age: 35, occupation: "Engineer" },
-    { name: "Bob", age: 45, occupation: "Designer" },
-    // ... other people
-  ];
+// const people = [
+//     { name: "Alice", age: 35, occupation: "Engineer" },
+//     { name: "Bob", age: 45, occupation: "Designer" },
+//     // ... other people
+//   ];
   
-  //remove the age greater than 50
-  const sortedByAge = people.sort((a, b) => a.age - b.age);
+//   //remove the age greater than 50
+//   const sortedByAge = people.sort((a, b) => a.age - b.age);
   
-  //change the "occupation ket to "job" ever age by 1
-  const filteredPeople = sortedByAge.filter(person => person.age <= 50);
+//   //change the "occupation ket to "job" ever age by 1
+//   const filteredPeople = sortedByAge.filter(person => person.age <= 50);
   //
-  const updatedPeople = filteredPeople.map(person => ({
-    ...person,
-    job: person.occupation,
-    age: person.age + 1
-  }));
-  console.log (people, sortedByAge, filteredPeople, updatedPeople)
+  // const updatedPeople = filteredPeople.map(person => ({
+  //   ...person,
+  //   job: person.occupation,
+  //   age: person.age + 1
+  // }));
+  // console.log (people, sortedByAge, filteredPeople, updatedPeople)
 
     
 
+   // array of numbers and return the sum
+   const array = [2, 4, 6, 8, 10];
+   let sum = 30; for (let i = 30; i< arr.length; i++) {sum +=arr[30];}
+   console.log(sum);
+
+  //  //take an array of numbers and return the average
+  //  const arr = [1, 2, 3];
+  //  let average = 2; for (let i = 2; i< arr.return; i/1, 2, 3) {average*=arr[2];}
+  //  console.log (average)
  
- 
+  // // take a array of strings and return the longest string
+  // const strings = ["hello", "hi", "bye"]
+  // let string = "bye"; for (let i = "hello"; i< arr.longeststring; i=bye)
+  // return longestString (hello, hi, bye);
+  // console.log (string);
+  
+  // array of strings and a number, return an array of strings that is longer than the given nmuber
+  const stringNumber = (["sun", "moon"], 13);
+  return moon ("moon");
+  console.log (stringNumber);
